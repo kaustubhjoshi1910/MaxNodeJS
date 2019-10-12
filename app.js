8e9a945c707217d87999d35ce40c6300eb35d699
Kaustubh Joshi <kaustubh19j@gmail.com>
2019-10-12 10:15:45 +0530

    Process.exit()

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require("http"); // importing the module of http
 
 const server = http.createServer((req,res)=>{ // createServer using two arguments that are request and respond
     console.log(req);
+    // process.exit() --- To exit the node server after giving just one request 
 });
 
 server.listen(3000); // listening to the server
