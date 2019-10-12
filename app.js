7f752248f670934df4fa760e1b280f718a531626
Kaustubh Joshi <kaustubh19j@gmail.com>
2019-10-12 12:11:12 +0530

    Redirecting the routes

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const http = require("http"); // importing the module of http
+const fs = require("fs");
 
-const server = http.createServer((req,res)=>{ // createServer using two arguments that are request and respond
-    console.log(req);
+const server = http.createServer((req, res) => { // createServer using two arguments that are request and respond
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write("<html>");
+        res.write("<head><title>Enter message</title></head>")
+        res.write("<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>");
+        res.write("</html>");
+        return res.end();
+
+    }
+
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync("message.txt","DUMMY");
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+
+    res.setHeader('content-type', 'text/html');
+    res.write("<html>");
+    res.write("<head><title>Starting Node JS</title></head>");
+    res.write("<body><h1>Response from the Server.</h1></body>");
+    res.write("</html>");
+    res.end();
+    // req.headers,req.url,req.method ---Important ones
     // process.exit() --- To exit the node server after giving just one request 
 });
 
